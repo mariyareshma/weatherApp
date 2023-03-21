f3cce250417be77c45e9b76300c79d85a7e5130f
Mariya Reshma <rreshma10653@gmail.com>
2023-03-21 12:39:14 +0530

    some changes in search page

--- lib/servive/current_weather.dart
+++ lib/servive/current_weather.dart
@@ -1,15 +1,17 @@
 import 'package:dio/dio.dart';
+import 'package:weather_app/model/area_model.dart';
 import 'package:weather_app/model/current.dart';
 
 var dio = Dio();
+SearchArea? latlon;
 
-Future<Main?> getCurrentWeather() async {
+Future<CurrentWeather?> getCurrentWeather() async {
   var response = await dio.get(
-      'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=67c3eb7d595b1778a9fd5a7cf78b8ce4');
+      'https://api.openweathermap.org/data/2.5/weather?lat=${latlon!.lat}&lon=${latlon!.lon}&appid=67c3eb7d595b1778a9fd5a7cf78b8ce4');
 
   if (response.statusCode == 200) {
     var placeJsons = response.data;
-    var place = Main.fromJson(placeJsons);
+    var place = CurrentWeather.fromJson(placeJsons);
     return place;
   }
 
--- lib/servive/hour_weather.dart
+++ lib/servive/hour_weather.dart
@@ -3,13 +3,13 @@ import 'package:weather_app/model/hour.dart';
 
 var dio = Dio();
 
-Future<Clouds?> getHourlyWeaather() async {
+Future<HourWeather?> getHourlyWeaather() async {
   var response = await dio.get(
       'api.openweathermap.org/data/2.5/forecast?lat=44.4&lon=89.9&appid=67c3eb7d595b1778a9fd5a7cf78b8ce4');
 
   if (response.statusCode == 200) {
     var placeJsons = response.data;
-    var place = Clouds.fromJson(placeJsons);
+    var place = HourWeather.fromJson(placeJsons);
     return place;
   }
 
